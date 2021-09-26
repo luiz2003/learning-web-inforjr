a5161f3b340e008599dfed3b2c4b87b22ff52b25
Luiz Valverde <luizcastroval321@gmail.com>
2021-09-26 12:16:42 -0300

    altura do menu hamburguer definida dinamicamendo com a DOM do js e usando variaveis do css

--- 3 - Zomentum/script.js
+++ 3 - Zomentum/script.js
@@ -1,8 +1,18 @@
+let header = document.getElementById('header')
+let nav = document.getElementById('nav')
+let root = document.documentElement;
+let headerHeight = header.offsetHeight - nav.offsetHeight
+console.log( headerHeight)
+root.style.setProperty('--headerHeight',headerHeight+"px")
+
+
 const btnMobile = document.getElementById('btnMbl')
 console.log(btnMobile)  
 function toggleMenu() {
+    const body = document.getElementById('body')
     const nav =  document.getElementById('nav')
     nav.classList.toggle('active')
+    body.classList.toggle('active')
 
     if(nav.classList.contains('active')){
         document.getElementById("menuImg").src="images/close-icon.svg";
