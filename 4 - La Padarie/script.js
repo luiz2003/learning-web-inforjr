5cf316714abe9c84eb2663dd77864a0203d6254f
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-03 20:04:01 -0300

    consertei o bug que fazia o modal parar de funcionar quando os cards renderizavam

--- 4 - La Padarie/script.js
+++ 4 - La Padarie/script.js
@@ -2,7 +2,7 @@ const addBtn = document.getElementById('addBtn')
 const cancelBtn = document.getElementById('cancelBtn')
 function toggleMenu() {
     const modal =  document.getElementById('modal')
-    modal.classList.add('active')
+    modal.classList.toggle('active')
 
 }
 function untoggleMenu() {
@@ -10,6 +10,8 @@ function untoggleMenu() {
     modal.classList.remove('active')
 
 }
+addBtn.addEventListener('click',toggleMenu)
+cancelBtn.addEventListener('click',untoggleMenu)
 
 let clientCards = [
     {
@@ -25,24 +27,22 @@ let clientCards = [
         id : 1
     }
 ]
+
 let queue = document.getElementById('queue')
 clientCards.forEach((card)=>{
     queue.innerHTML += 
     `
+    
     <div class="client-card">
     <div class="client-card-text">
-        <h2 id="clientName">Alexandre Shyjada Sousa</h2>
+        <h2 id="clientName">${card.clientName}</h2>
         <div class="client-card-info">
-            <h3>Total de pães: <p><span id="clientBread">50</span>pães</p> </h3> 
-            <h3>Total a pagar: <p><span id="clientInflux">R$ 25,00</span></p></h3>
+            <h3>Total de pães: <p><span id="clientBread">${card.breadAmount}</span>pães</p> </h3> 
+            <h3>Total a pagar: <p><span id="clientInflux">R$ ${(card.breadAmount)/2}</span></p></h3>
         </div>
     </div>
    <img id="deleteBtn" src="images/delete-icon.svg" alt="">
 </div>
     `
 })
-addBtn.addEventListener('click',() => {
-
-})
-addBtn.addEventListener('click',toggleMenu)
-cancelBtn.addEventListener('click',untoggleMenu)
+ 
