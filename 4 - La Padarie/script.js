aa41535a0d4458f6e1a73f58257de301dbaab24b
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-03 19:10:25 -0300

    cards renderizam com js

--- 4 - La Padarie/script.js
+++ 4 - La Padarie/script.js
@@ -10,5 +10,39 @@ function untoggleMenu() {
     modal.classList.remove('active')
 
 }
+
+let clientCards = [
+    {
+        clientName: "Alexandre Shyjada Sousa",
+        breadAmount: 50,
+        clientInflux: 25,
+        id : 0
+    },
+    {
+        clientName: "Luiz Valverde",
+        breadAmount: 5,
+        clientInflux:25,
+        id : 1
+    }
+]
+let queue = document.getElementById('queue')
+clientCards.forEach((card)=>{
+    queue.innerHTML += 
+    `
+    <div class="client-card">
+    <div class="client-card-text">
+        <h2 id="clientName">Alexandre Shyjada Sousa</h2>
+        <div class="client-card-info">
+            <h3>Total de pães: <p><span id="clientBread">50</span>pães</p> </h3> 
+            <h3>Total a pagar: <p><span id="clientInflux">R$ 25,00</span></p></h3>
+        </div>
+    </div>
+   <img id="deleteBtn" src="images/delete-icon.svg" alt="">
+</div>
+    `
+})
+addBtn.addEventListener('click',() => {
+
+})
 addBtn.addEventListener('click',toggleMenu)
 cancelBtn.addEventListener('click',untoggleMenu)
