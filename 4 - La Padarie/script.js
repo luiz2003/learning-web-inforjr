8e1ecdbf33ca8569ab4ed82b3731ec168840a88f
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-04 12:50:04 -0300

    Funcionalidade de adicao de cards funcional

--- 4 - La Padarie/script.js
+++ 4 - La Padarie/script.js
@@ -1,12 +1,16 @@
 const addBtn = document.getElementById('addBtn')
 const cancelBtn = document.getElementById('cancelBtn')
+const modal =  document.getElementById('modal')
 function toggleMenu() {
-    const modal =  document.getElementById('modal')
+    
     modal.classList.toggle('active')
 
 }
 function untoggleMenu() {
-    const modal =  document.getElementById('modal')
+    let nameInput = document.getElementById('nameInput')
+    let breadInput = document.getElementById('breadInput')
+    nameInput.value =""
+    breadInput.value=""
     modal.classList.remove('active')
 
 }
@@ -14,35 +18,50 @@ addBtn.addEventListener('click',toggleMenu)
 cancelBtn.addEventListener('click',untoggleMenu)
 
 let clientCards = [
-    {
-        clientName: "Alexandre Shyjada Sousa",
-        breadAmount: 50,
-        clientInflux: 25,
-        id : 0
-    },
-    {
-        clientName: "Luiz Valverde",
-        breadAmount: 5,
-        clientInflux:25,
-        id : 1
-    }
-]
-
-let queue = document.getElementById('queue')
-clientCards.forEach((card)=>{
-    queue.innerHTML += 
-    `
     
-    <div class="client-card">
-    <div class="client-card-text">
-        <h2 id="clientName">${card.clientName}</h2>
-        <div class="client-card-info">
-            <h3>Total de pães: <p><span id="clientBread">${card.breadAmount}</span>pães</p> </h3> 
-            <h3>Total a pagar: <p><span id="clientInflux">R$ ${(card.breadAmount)/2}</span></p></h3>
+]
+function addClientCard(){
+    let clientName = document.getElementById('nameInput').value
+    let breadAmount = document.getElementById('breadInput').value
+    let nameInput = document.getElementById('nameInput')
+    let breadInput = document.getElementById('breadInput')
+    
+    if(clientName != "" && breadAmount>0){
+        clientCards.push(
+            {
+                clientName,
+                breadAmount,
+                clientInflux: breadAmount/2,
+                id: 0
+            }
+        )
+        nameInput.value =""
+        breadInput.value=""
+        modal.classList.remove('active')
+    }
+    else{
+        window.alert("Por favor insira valores válidos")
+        nameInput.value =""
+        breadInput.value=""
+    }
+    let queue = document.getElementById('queue')
+    clientCards.forEach((card)=>{
+        queue.innerHTML += 
+        `
+        
+        <div class="client-card">
+        <div class="client-card-text">
+            <h2 id="clientName">${card.clientName}</h2>
+            <div class="client-card-info">
+                <h3>Total de pães: <p><span id="clientBread">${card.breadAmount}</span>pães</p> </h3> 
+                <h3>Total a pagar: <p><span id="clientInflux">R$ ${(card.breadAmount)/2}</span></p></h3>
+            </div>
         </div>
+    <img id="deleteBtn" src="images/delete-icon.svg" alt="">
     </div>
-   <img id="deleteBtn" src="images/delete-icon.svg" alt="">
-</div>
-    `
-})
+        `
+    })
+}
+
+
  
