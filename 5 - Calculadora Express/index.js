f40c91bdd77d7b943a876d0aff5db0043a7a64e4
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-09 10:46:01 -0300

    basic math with routing done, still gotta figure the form out

--- 5 - Calculadora Express/index.js
+++ 5 - Calculadora Express/index.js
@@ -4,13 +4,37 @@ const app = express()
 const port = 3000
 const path = require('path')
 
+function math(num1,num2){
+
+}
 //static folder
 app.use(express.static(path.join(__dirname,'public')))
 
-//routing 
-/* app.get('/api/members',(req,res) =>{
-  res.send('')
-}) */
+//routing and math
+app.get('/adicionar/:num1/:num2',(req,res)=>{
+  let num1 = parseFloat(req.params.num1)
+  let num2 = parseFloat(req.params.num2)
+  result = (num1+num2).toString()
+  res.send(result)
+})
+app.get('/subtrair/:num1/:num2',(req,res)=>{
+  let num1 = parseFloat(req.params.num1)
+  let num2 = parseFloat(req.params.num2)
+  result = (num1-num2).toString()
+  res.send(result)
+})
+app.get('/multiplicar/:num1/:num2',(req,res)=>{
+  let num1 = parseFloat(req.params.num1)
+  let num2 = parseFloat(req.params.num2)
+  result = (num1*num2).toString()
+  res.send(result)
+})
+app.get('/dividir/:num1/:num2',(req,res)=>{
+  let num1 = parseFloat(req.params.num1)
+  let num2 = parseFloat(req.params.num2)
+  result = (num1/num2).toString()
+  res.send(result)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
