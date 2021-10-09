3e7c8fc440e8a73696e5abfb5e4f2e90da564aac
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-09 09:34:35 -0300

    basic form html done

--- 5 - Calculadora Express/index.js
+++ 5 - Calculadora Express/index.js
@@ -2,10 +2,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+//routing 
+/* app.get('/api/members',(req,res) =>{
+  res.send('')
+}) */
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
