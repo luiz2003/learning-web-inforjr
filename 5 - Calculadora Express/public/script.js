938d018dd5adcafb98b11f5d9b67b73643504108
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-13 18:10:56 -0300

    fade in and out effects for selecting operations

--- 5 - Calculadora Express/public/script.js
+++ 5 - Calculadora Express/public/script.js
@@ -4,29 +4,80 @@ var sub = false;
 var mul = false;
 var div = false; 
 
+let addBtn = document.getElementById('addBtn')
+let subBtn = document.getElementById('subBtn')
+let mulBtn = document.getElementById('mulBtn')
+let divBtn = document.getElementById('divBtn')
+
+let startColor = {r: 15, g:113, b:115};
+let endColor   = {r:  10, g: 71, b: 72};
+
+function reverseFade(){
+    buttons = [ 
+        {
+            bool : sum,
+            btn : addBtn
+        },
+        {
+            bool : sub,
+            btn : subBtn
+        },
+        {
+            bool : mul,
+            btn : mulBtn
+        },
+        {
+            bool : div,
+            btn : divBtn
+        }
+    ]
+    
+    buttons.forEach(button => {
+        if(button.bool){
+            fade(button.btn,endColor,startColor,150)
+        }
+    });
+}
+
 function sumSelect(){
+    reverseFade()
+    
     sum = true;
     sub = false;
     mul = false;
     div = false; 
+    
+    fade(addBtn,startColor,endColor,150)    
 }
 function subSelect(){
+    reverseFade()
+    
     sum = false;
     sub = true;
     mul = false;
-    div = false; 
+    div = false;
+
+    fade(subBtn,startColor,endColor,150)   
 }
 function mulSelect(){
+    reverseFade()
+    
     sum = false;
     sub = false;
     mul = true;
-    div = false; 
+    div = false;
+
+    fade(mulBtn,startColor,endColor,150)   
 }
 function divSelect(){
+    reverseFade()
+    
     sum = false;
     sub = false;
     mul = false;
-    div = true; 
+    div = true;
+
+    fade(divBtn,startColor,endColor,150)   
 }
 //redirect to result script
 function calculate(){
@@ -52,3 +103,27 @@ function calculate(){
     }
 
 }
+
+//fade to new color when selected script
+lerp = function(a, b, u) {
+    return (1 - u) * a + u * b;
+};
+
+fade = function(element, start, end, duration) {
+    var interval = 10;
+    var steps = duration / interval;
+    var step_u = 1.0 / steps;
+    var u = 0.0;
+    var theInterval = setInterval(function() {
+        if (u >= 1.0) {
+            clearInterval(theInterval);
+        }
+        var r = Math.round(lerp(start.r, end.r, u));
+        var g = Math.round(lerp(start.g, end.g, u));
+        var b = Math.round(lerp(start.b, end.b, u));
+        var colorname = 'rgb(' + r + ',' + g + ',' + b + ')';
+        element.style.setProperty('background-color', colorname);
+        u += step_u;
+    }, interval);
+    console.log('interpolation!')
+};
