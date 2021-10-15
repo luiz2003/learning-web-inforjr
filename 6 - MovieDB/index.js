fd9fcb8b4aa3fecfb2f78fb8732f67960253d11e
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-15 19:16:14 -0300

    api creation

--- 6 - MovieDB/index.js
+++ 6 - MovieDB/index.js
@@ -6,3 +6,14 @@ const fetch = require('cross-fetch');
 
 //static folder
 app.use(express.static(path.join(__dirname,'public')))
+
+//api creation
+app.get('/api',  async (req,res)=>{
+    const response = await fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1')
+    const data = await response.json()
+    res.send(data)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
