924b277d13d3f72a645f258f2afa31ffdf974582
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-15 21:13:30 -0300

    fetching all necessary data and rendering it on html, including imgs

--- 6 - MovieDB/public/script.js
+++ 6 - MovieDB/public/script.js
@@ -1,3 +1,4 @@
+//rendering basic movie info on html
 fetch('http://localhost:3000/api')
     .then(res => { 
        return res.json()
@@ -5,8 +6,15 @@ fetch('http://localhost:3000/api')
     .then(data => {
         data.results.forEach(movie => {
             document.getElementById('moviesArea').innerHTML+= `
-            <p>${movie.original_title}</p>
-
+            <img class="poster"src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+            <p>${movie.title}</p>
+            <p>${movie.title !== movie.original_title ? movie.original_title : ''}</p>
+            <p>moviez</p>
+            <p>release date${movie.release_date}</p>
+            <p>original language: ${movie.original_language}</p>
+            <p>vote average: ${movie.vote_average}</p>
+            <p>${movie.overview}</p>
+            <br>
             ` 
         });
     })
