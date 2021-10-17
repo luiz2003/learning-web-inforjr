9695c2ccc5b6ae92388e9a7b289814c5dec532f1
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-16 22:16:17 -0300

    some more basic styling and preparation for later stuff

--- 6 - MovieDB/public/script.js
+++ 6 - MovieDB/public/script.js
@@ -6,15 +6,20 @@ fetch('http://localhost:3000/api')
     .then(data => {
         data.results.forEach(movie => {
             document.getElementById('moviesArea').innerHTML+= `
-            <img class="poster"src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-            <p>${movie.title}</p>
-            <p>${movie.title !== movie.original_title ? movie.original_title : ''}</p>
-            <p>moviez</p>
-            <p>release date${movie.release_date}</p>
-            <p>original language: ${movie.original_language}</p>
-            <p>vote average: ${movie.vote_average}</p>
-            <p>${movie.overview}</p>
-            <br>
+                <div class="movie-card">
+                    <div class="title">
+                        <img class="poster"src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+                        <p>${movie.title}</p>
+                    </div>
+                    <div class= "description" id="${data.results.indexOf(movie,0)}">
+                        <p>${movie.title !== movie.original_title ? movie.original_title : ''}</p>
+                        <p>moviez</p>
+                        <p>release date${movie.release_date}</p>
+                        <p>original language: ${movie.original_language}</p>
+                        <p>vote average: ${movie.vote_average}</p>
+                        <p>${movie.overview}</p>
+                    </div>
+                </div>
             ` 
         });
     })
