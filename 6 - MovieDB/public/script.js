8f630b88e53d6643bf44e03d97d99c3f39a8e3f5
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-17 20:33:52 -0300

    adding extra info dropdown area

--- 6 - MovieDB/public/script.js
+++ 6 - MovieDB/public/script.js
@@ -1,15 +1,19 @@
+var cardsHtml;
 //rendering basic movie info from api on html
 fetch('http://localhost:3000/api')
     .then(res => { 
        return res.json()
     })
     .then(data => {
+        
         data.results.forEach(movie => {
+            
             document.getElementById('moviesArea').innerHTML+= `
                 <div class="movie-card">
                     <div class="title">
                         <img class="poster"src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
                         <p>${movie.title}</p>
+                        <button onclick="expandCard(${data.results.indexOf(movie,0)})"><img src="images/arrow-down-sign-to-navigate.png" alt=""></button>
                     </div>
                     <div class= "description" id="${data.results.indexOf(movie,0)}">
                         <p>${movie.title !== movie.original_title ? movie.original_title : ''}</p>
@@ -22,4 +26,25 @@ fetch('http://localhost:3000/api')
                 </div>
             ` 
         });
-    })
+        
+        //getting all the movie cards as html collection 
+        cardsHtml = document.getElementsByClassName('movie-card') 
+        
+        //converting the html collection to an array
+        let cards = Array.from(cardsHtml)
+        console.log(cards)
+        
+       /*  //going to trough the array and adding "the show more information" functionality
+        cards.forEach((card) =>{
+            card.addEventListener("click",function expandCard(){
+                console.log(cards);
+            })
+        }) */
+       
+})
+
+function expandCard(id){
+    let active = document.getElementById(`${id}`)
+    active.classList.toggle('active')
+    console.log("active!")
+}
