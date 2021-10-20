1b30f2cabb7d5d9f394f44bfdff846c05a1d4fc4
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-20 18:50:12 -0300

    removing some useless code and organizing js + minor adjustments

--- 6 - MovieDB/public/script.js
+++ 6 - MovieDB/public/script.js
@@ -1,21 +1,4 @@
-var cardsHtml;
-//page selection script
-document.getElementById('pageSelect').value = 1;
-
-pageSelect = document.getElementById('pageSelect')
-
-let page = 1;
-render(page)
-document.getElementById('select').addEventListener('click',()=>{
-    if(pageSelect.value >=1 && pageSelect.value<=500){
-        render(pageSelect.value)
-    }
-    else{
-        document.getElementById('moviesArea').innerHTML = `<h1>This page does not exist :[</h1>`
-    }
-})
-
-//rendering basic movie info from api on html
+//rendering the page based on the api on html
 function render(page){
     fetch( `http://localhost:3000/api/${page}`)
     .then(res => { 
@@ -45,29 +28,29 @@ function render(page){
                 </div>
             ` 
         });
-        
-        //getting all the movie cards as html collection 
-        cardsHtml = document.getElementsByClassName('movie-card') 
-        
-        //converting the html collection to an array
-        let cards = Array.from(cardsHtml)
-        console.log(cards)
-        
-       /*  //going to trough the array and adding "the show more information" functionality
-        cards.forEach((card) =>{
-            card.addEventListener("click",function expandCard(){
-                console.log(cards);
-            })
-        }) */
-       
-})
+    })
 }
 
 function expandCard(id){
     let active = document.getElementById(`${id}`)
     active.classList.toggle('active')
-    console.log("active!")
 }
+
+//page selection script
+document.getElementById('pageSelect').value = 1;
+
+pageSelect = document.getElementById('pageSelect')
+
+let page = 1;
+render(page)
+document.getElementById('select').addEventListener('click',()=>{
+    if(pageSelect.value >=1 && pageSelect.value<=500){
+        render(pageSelect.value)
+    }
+    else{
+        document.getElementById('moviesArea').innerHTML = `<h1>This page does not exist :[</h1>`
+    }
+})
 
 // movieDB search engine
 searchBar = document.getElementById('searchBar')
@@ -76,10 +59,9 @@ searchBar.addEventListener('keydown',(event)=>{
     query = searchBar.value;
     if(event.keyCode=== 13 && query == ""){
         window.alert("Please input your query")
-        console.log('oi')
     }
     else if(event.keyCode===13){
-        console.log('xau')
+       
         fetch( `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${query}`)
         .then(res => { 
            return res.json()
@@ -98,7 +80,7 @@ searchBar.addEventListener('keydown',(event)=>{
                         </div>
                         <div class= "description" id="${data.results.indexOf(movie,0)}">
                             <p>${movie.title !== movie.original_title ? movie.original_title : ''}</p>
-                            <p>moviez</p>
+                            <p class="logo">moviez</p>
                             <p><h2>release date:</h2> ${movie.release_date}</p>
                             <p><h2>original language:</h2> ${movie.original_language}</p>
                             <p><h2>vote average:</h2> ${movie.vote_average}</p>
