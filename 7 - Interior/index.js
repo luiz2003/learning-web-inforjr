16b5c419dd099e3df1afac13bc4db4aad78eaefc
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-22 21:38:04 -0300

    initial commit: all depedencies installed + email sending setup is working

---
+++ 7 - Interior/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()  
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+const nodemailer = require('nodemailer') 
+
+//static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+//create transpoter (connect to email service)
+let transporter = nodemailer.createTransport({
+    service :  'gmail', 
+    auth: {
+        user : process.env.EMAIL_ID, //get user name from .env file
+        pass: process.env.PASSWORD //get password from .env file
+    }
+})
+
+//create email template
+let mailOptions = {
+   from: '"Interior Desings" <interior.design.official.infojr@gmail.com>', // sender address
+   to: "luizcastroval321@gmail.com", // receiver address
+   subject: "Ooiii", // Subject line
+   text: "isso aqui funciona?", // plain text body
+   html: "<b>Thanks for using our services</b>", // html body
+};
+
+//send email
+transporter.sendMail(mailOptions, (err,data)=>{
+    if(err){
+        console.log('error',err)
+    }
+    else{
+        console.log('email sent correctly')
+    }
+})
+
+//port listening    
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
