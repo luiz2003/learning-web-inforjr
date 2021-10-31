ccc9b2ff8d363af4dd4b06943d86aa735243faa2
Luiz Valverde <luizcastroval321@gmail.com>
2021-10-31 17:04:00 -0300

    font importing + desktop header

---
+++ 8 - Travelnow/Travelnow/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './style.css'
+
+export default function Header() {
+    return (
+        <div>
+            <header>
+                <img src="images/logo.svg" alt="" />
+                
+                <nav>
+                    <a href="">Discover</a>
+                    <a href="">Destination</a>
+                    <a href="">About us</a>
+                </nav>
+
+                <button>Book a Tour</button>
+            </header>
+        </div>
+    )
+}
