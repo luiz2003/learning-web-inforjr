1ed66995bba830628b8a71390b7c0639fe8059a9
Luiz Valverde <luizcastroval321@gmail.com>
2021-11-01 23:10:32 -0300

    Discover section done and responsive

--- 8 - Travelnow/Travelnow/src/main.jsx
+++ 8 - Travelnow/Travelnow/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './Components/Header/Header'
+import Discover from './Components/Discover/Discover'
 
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <Discover />
   </React.StrictMode>,
   document.getElementById('root')
 )
