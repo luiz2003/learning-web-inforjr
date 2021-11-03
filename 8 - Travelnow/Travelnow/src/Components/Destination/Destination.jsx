ddff37d082d94042fbeedf36ddb85b3aa1dc9c05
Luiz Valverde <luizcastroval321@gmail.com>
2021-11-02 21:35:08 -0300

    discover section halfway done

---
+++ 8 - Travelnow/Travelnow/src/Components/Destination/Destination.jsx
@@ -0,0 +1,63 @@
+import './style.css'
+
+export default function Destination() {
+    return (
+        <div className = "destination">
+
+            <div className="hero2">
+
+                <img src="images/destination.svg" alt="" />
+
+                <aside>
+                  
+                    <div className="aside-title">
+                        <h1>Travel to any corner of the World</h1>
+
+                        <p>
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of type.
+                        </p>
+                    </div>
+
+                    <div className="info">
+                        <div className="info-text">
+                            <div className="info-block">
+                                <h2>20</h2>
+                                <p>Years Experience</p>
+                            </div>
+
+                            <div className="info-block">
+                                <h2>55</h2>
+                                <p>Destination Collaboration</p>
+                            </div>
+
+                            <div className="info-block">
+                                <h2>600+</h2>
+                                <p>Tourist Destination</p>
+                            </div>
+
+                        </div>
+
+                        <button>Explore Destination</button>
+                    </div>
+                </aside>
+
+            </div>
+
+            <div className="cards">
+                <div className="card">
+                    
+                </div>
+
+                <div className="card">
+
+                </div>
+
+                <div className="car">
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- 8 - Travelnow/Travelnow/src/main.jsx
+++ 8 - Travelnow/Travelnow/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './Components/Header/Header'
 import Discover from './Components/Discover/Discover'
+import Destination from './Components/Destination/Destination'
 
 
 
@@ -9,6 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Discover />
+    <Destination />
   </React.StrictMode>,
   document.getElementById('root')
 )
